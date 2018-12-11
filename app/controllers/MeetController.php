db02d7b01ad422f073e267dfe56cd75b587acae5
Jirakit <jirakit607@gmail.com>
2018-12-12 00:03:05 +0700

    fix all page

---
+++ app/controllers/MeetController.php
@@ -0,0 +1,15 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class  MeetController extends Controller
+{
+    public function indexAction()
+    {
+        
+        
+    }
+ 
+
+
+}
---
+++ app/controllers/OtherController.php
@@ -0,0 +1,15 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class  OtherController extends Controller
+{
+    public function indexAction()
+    {
+        
+        
+    }
+ 
+
+
+}
